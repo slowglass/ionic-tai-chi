9431a3d7dd01a199331463afcfb7e242abf8978d
Chris Davies <cdavies@itrsgroup.com>
2017-04-16 19:10:03 +0100

    Made SoundPlayer work with both Native & HTML5 Audio

--- src/providers/sound-player.ts
+++ src/providers/sound-player.ts
@@ -1,21 +1,45 @@
 import { Injectable } from '@angular/core';
+import { NativeAudio } from '@ionic-native/native-audio';
+import { Device } from '@ionic-native/device';
 
 declare var Howl:any; 
+export class SoundPlayerNative {
+	constructor(private audio: NativeAudio) { }
+	msg(name:string, result: string) { console.log("Sound " + name + "Loaded"); }
+	addSound(name: string, url: string) {
+		this.audio.preloadSimple(name,url).then(
+	  		() => this.msg(name, 'Loaded'),
+	  		() => this.msg(name, 'Failed to Load!!')
+	  	);
+		//this.sounds[name] = new Howl({ src: [url] });
+	  }
+	  play(name:string) { this.audio.play(name); }
+}
+
+export class SoundPlayerHTML5 {
+	private sounds: Map<string,any>;
+	constructor() { this.sounds = new Map<string,any>(); }
+	msg(name:string, result: string) { console.log("Sound " + name + "Loaded"); }
+	addSound(name: string, url: string) {
+	  	this.sounds[name] = new Howl({ src: [url] });
+	}
+	play(name:string) { this.sounds[name].play(); }
+}
 
 @Injectable()
 export class SoundPlayer {
 
-  private sounds :Map<string,any>;
-
-  constructor() {
+  private player: any;
+  constructor(
+  	private device: Device,
+  	private audio: NativeAudio) {
+  	if (this.device.platform == null)
+  		this.player = new SoundPlayerHTML5();
+  	else
+  		this.player = new SoundPlayerNative(audio);
   }
 
-  addSound(name: string, url: string) {
-	this.sounds[name] = new Howl({ src: [url] });
-  }
-
-  play(name:string) {
-  	this.sounds[name].play();
-  }
-  
+  addSound(name: string, url: string) { this.player.addSound(name, url); }
+  play(name: string) { this.player.play(name); }
 }
+
