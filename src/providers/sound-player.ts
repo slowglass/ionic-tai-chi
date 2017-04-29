89e946c9726c7c113132cd1b8d2bc8dd2f6d6239
Chris Davies <Christopher.James.Davies@gmail.com>
2017-04-29 17:25:52 +0100

    Style fixes

--- src/providers/sound-player.ts
+++ src/providers/sound-player.ts
@@ -5,40 +5,41 @@ import { NativeAudio } from '@ionic-native/native-audio';
 
 declare var Howl:any; 
 export class SoundPlayerNative {
-	private playing: Map<string,boolean> = new Map<string,boolean>();
+	private playing: Map<string, boolean> = new Map<string, boolean>();
 	constructor(private audio: NativeAudio) { }
-	msg(name:string, result: string) { console.log("Native Sound " + name + " "+result); }
+	msg(name: string, result: string) { console.log("Native Sound " + name + " " + result); }
 	addSound(name: string, url: string) {
-		this.audio.preloadSimple(name,url).then(
-	  		() => this.msg(name, 'Loaded'),
-	  		() => this.msg(name, 'Failed to Load!!')
-	  	);
-	  }
-	  play(name:string) {
-		  if (this.playing[name] == true) return; 
-		  this.playing[name] = true;
-		  this.audio.play(name).then(
-			  (res) => { this.playing[name] = false; }, 
-			  (err) => { this.playing[name] = false; });
-		 }
+		this.audio.preloadSimple(name, url).then(
+			() => this.msg(name, 'Loaded'),
+			() => this.msg(name, 'Failed to Load!!')
+		);
+	}
+	play(name: string) {
+		if (this.playing[name] == true) return;
+		this.playing[name] = true;
+		this.audio.play(name).then(
+			(res) => { this.playing[name] = false; },
+			(err) => { this.playing[name] = false; });
+	}
 }
 
 export class SoundPlayerHTML5 {
-	private playing: Map<string,boolean> = new Map<string,boolean>();
-	private sounds: Map<string,any>  = new Map<string,any>();
+	private playing: Map<string, boolean> = new Map<string, boolean>();
+	private sounds: Map<string, any> = new Map<string, any>();
 	constructor() { this.sounds }
-	msg(name:string, result: string) { console.log("HTML5 Sound " + name + " " + result); }
-	soundPlaying(name: string, status:boolean) { this.playing[name] = status; }
+	msg(name: string, result: string) { console.log("HTML5 Sound " + name + " " + result); }
+	soundPlaying(name: string, status: boolean) { this.playing[name] = status; }
 	addSound(name: string, url: string) {
-		var self=this;
-	  	this.sounds[name] = new Howl({ 
-			  src: [url], 
-			  onload: () => this.msg(name, 'Loaded'),
-			  onloaderr: () => this.msg(name, 'Failed to Load!!'),
-			  onend:  () => this.soundPlaying(name, true)});
+		var self = this;
+		this.sounds[name] = new Howl({
+			src: [url],
+			onload: () => this.msg(name, 'Loaded'),
+			onloaderr: () => this.msg(name, 'Failed to Load!!'),
+			onend: () => this.soundPlaying(name, true)
+		});
 	}
-	play(name:string) { 
-		if (this.playing[name] == true) return; 
+	play(name: string) {
+		if (this.playing[name] == true) return;
 		this.playing[name] = true;
 		this.sounds[name].play();
 	}
