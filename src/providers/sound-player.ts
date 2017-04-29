18f581cdc241d04afcca6a0ee817d02a335e307c
Chris Davies <Christopher.James.Davies@gmail.com>
2017-04-29 17:16:57 +0100

    SoundPlayer now ignores play for a sound if it is already playing the sound

--- src/providers/sound-player.ts
+++ src/providers/sound-player.ts
@@ -1,29 +1,47 @@
 import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular';
 import { NativeAudio } from '@ionic-native/native-audio';
-import { Device } from '@ionic-native/device';
+//import { Device } from '@ionic-native/device';
 
 declare var Howl:any; 
 export class SoundPlayerNative {
+	private playing: Map<string,boolean> = new Map<string,boolean>();
 	constructor(private audio: NativeAudio) { }
-	msg(name:string, result: string) { console.log("Sound " + name + "Loaded"); }
+	msg(name:string, result: string) { console.log("Native Sound " + name + " "+result); }
 	addSound(name: string, url: string) {
 		this.audio.preloadSimple(name,url).then(
 	  		() => this.msg(name, 'Loaded'),
 	  		() => this.msg(name, 'Failed to Load!!')
 	  	);
-		//this.sounds[name] = new Howl({ src: [url] });
 	  }
-	  play(name:string) { this.audio.play(name); }
+	  play(name:string) {
+		  if (this.playing[name] == true) return; 
+		  this.playing[name] = true;
+		  this.audio.play(name).then(
+			  (res) => { this.playing[name] = false; }, 
+			  (err) => { this.playing[name] = false; });
+		 }
 }
 
 export class SoundPlayerHTML5 {
-	private sounds: Map<string,any>;
-	constructor() { this.sounds = new Map<string,any>(); }
-	msg(name:string, result: string) { console.log("Sound " + name + "Loaded"); }
+	private playing: Map<string,boolean> = new Map<string,boolean>();
+	private sounds: Map<string,any>  = new Map<string,any>();
+	constructor() { this.sounds }
+	msg(name:string, result: string) { console.log("HTML5 Sound " + name + " " + result); }
+	soundPlaying(name: string, status:boolean) { this.playing[name] = status; }
 	addSound(name: string, url: string) {
-	  	this.sounds[name] = new Howl({ src: [url] });
+		var self=this;
+	  	this.sounds[name] = new Howl({ 
+			  src: [url], 
+			  onload: () => this.msg(name, 'Loaded'),
+			  onloaderr: () => this.msg(name, 'Failed to Load!!'),
+			  onend:  () => this.soundPlaying(name, true)});
 	}
-	play(name:string) { this.sounds[name].play(); }
+	play(name:string) { 
+		if (this.playing[name] == true) return; 
+		this.playing[name] = true;
+		this.sounds[name].play();
+	}
 }
 
 @Injectable()
@@ -31,12 +49,12 @@ export class SoundPlayer {
 
   private player: any;
   constructor(
-  	private device: Device,
-  	private audio: NativeAudio) {
-  	if (this.device.platform == null)
-  		this.player = new SoundPlayerHTML5();
-  	else
+  	platform: Platform,
+  	audio: NativeAudio) {
+  	if (platform.is('cordova'))
   		this.player = new SoundPlayerNative(audio);
+  	else
+  		this.player = new SoundPlayerHTML5();
   }
 
   addSound(name: string, url: string) { this.player.addSound(name, url); }
