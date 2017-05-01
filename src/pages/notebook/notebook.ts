2cb3dd257e8e48a2632ccfb37cebb3a72615bd06
Chris Davies <Christopher.James.Davies@gmail.com>
2017-05-01 08:09:57 +0100

    Added Notebook sub pages

--- src/pages/notebook/notebook.ts
+++ src/pages/notebook/notebook.ts
@@ -1,22 +1,89 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
+import { NotesPage } from '../notes/notes';
+import { Dialogs } from '@ionic-native/dialogs';
+import { Dropbox } from '../../providers/dropbox';
 
-/*
-  Generated class for the Notebook page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-notebook',
   templateUrl: 'notebook.html'
 })
-export class NotebookPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+export class NotebookPage {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public dropbox: Dropbox,
+    public loadingCtrl: LoadingController,
+    private dialogs: Dialogs) { }
+
+  depth: number = 0;
+  files: Array<any>;
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad NotebookPage');
+    this.files = [];
   }
 
+  dropboxLogin() {
+      if (this.dropbox.isLoggedIn()) return;
+
+      let loading = this.loadingCtrl.create({ content: 'Syncing from Dropbox...' });
+      this.dropbox.login().then(
+        (success) => { 
+          loading.dismiss(); 
+          console.log('Logged in'); 
+          this.openFolder("");
+        },
+        (err) => {
+          this.dialogs.alert('Hello world')
+            .then(() => loading.dismiss())
+            .catch(e => console.log('Error displaying dialog', e));
+        });
+  }
+
+  ionViewDidEnter() {
+      this.dropboxLogin();
+  }
+
+  openFolder(path) {
+    let loading = this.loadingCtrl.create({
+      content: 'Syncing from Dropbox...'
+    });
+
+    loading.present(loading);
+    this.dropbox.getFolders(path).subscribe(
+      (data) => {
+        this.files = data.entries;
+        this.depth++;
+        loading.dismiss();
+      }, 
+      (err) => { 
+        loading.dismiss();
+        console.log(err); 
+      }
+    );
+  }
+
+  openFile(idx) {
+    let file = this.files[idx];
+    console.log("File Info:"+JSON.stringify(file));
+    this.navCtrl.push(NotesPage, { note: file.path_lower });
+  }
+
+  goBack() {
+    let loading = this.loadingCtrl.create({
+      content: 'Syncing from Dropbox...'
+    });
+
+    loading.present(loading);
+
+    this.dropbox.goBackFolder().subscribe(data => {
+      this.files = data.entries;
+      this.depth--;
+      loading.dismiss();
+    }, err => {
+      console.log(err);
+    });
+
+  }
 }
---
+++ src/pages/notes/notes.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
+import { Dialogs } from '@ionic-native/dialogs';
+import { Dropbox } from '../../providers/dropbox';
+
+declare var micromarkdown:any;
+
+@Component({
+  selector: 'page-notes',
+  templateUrl: 'notes.html'
+})
+export class NotesPage {
+  private text:string = "Need to load";
+  private md:string;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public dropbox: Dropbox,
+    public loadingCtrl: LoadingController,
+    private dialogs: Dialogs) {}
+
+    
+  ionViewWillEnter() {
+    this.text = "Need to load";
+  }
+
+  ionViewDidEnter() {
+      let note  = this.navParams.get('note');
+      let loading = this.loadingCtrl.create({ content: 'Syncing from Dropbox...' });
+      this.dropbox.login().then(
+        (success) => { 
+          loading.dismiss(); 
+          console.log('Logged in'); 
+          this.open(note);
+        },
+        (err) => {
+          this.dialogs.alert('Hello world')
+            .then(() => loading.dismiss())
+            .catch(e => console.log('Error displaying dialog', e));
+        });
+      
+  }
+  
+  open(path) {
+    let loading = this.loadingCtrl.create({ content: 'Syncing from Dropbox...' });
+
+    loading.present(loading);
+    
+    this.dropbox.getFile(path).subscribe(
+      (data) => {
+        this.text = data;
+        this.md = micromarkdown.parse(data);
+        loading.dismiss();
+      }, 
+      (err) => { console.log(err); }
+    );
+  }
+
+
+}
