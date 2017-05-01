2a540fbda691e40616db239b7552f2f3846aeaa9
Chris Davies <Christopher.James.Davies@gmail.com>
2017-05-01 08:16:29 +0100

    Updated App Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,28 @@
+/* Angular Modules */
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 
-import { IonicStorageModule } from '@ionic/storage';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { NativeAudio } from '@ionic-native/native-audio';
-import { Device } from '@ionic-native/device';
+/* Third Party Angular Modules */
+import { ElasticModule } from 'angular2-elastic';
 
+/* Ionic Modules */
+import { IonicStorageModule } from '@ionic/storage';
+
+/* Ionic Native */  
+import { AppVersion } from '@ionic-native/app-version';  
+import { Device } from '@ionic-native/device';
+import { Dialogs } from '@ionic-native/dialogs';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { NativeAudio } from '@ionic-native/native-audio';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+/* Directives */
+
+/* Components */
+import { YinYangTimer } from '../components/yin-yang/yin-yang'
+
+/* Pages */
 import { MyApp } from './app.component';
 
 import { HomePage } from '../pages/home/home';
@@ -20,53 +36,60 @@ import { TimerConfigPage } from '../pages/timer-config/timer-config';
 import { FormsPage } from '../pages/forms/forms';
 import { FormPage } from '../pages/form/form';
 
-
 import { SwimmingDragonPage } from '../pages/swimming-dragon/swimming-dragon';
 import { SwimmingDragonTimerTab } from '../pages/swimming-dragon-timer/swimming-dragon-timer'
 import { SwimmingDragonSummaryStatsTab } from '../pages/swimming-dragon-summary-stats/swimming-dragon-summary-stats'
 import { SwimmingDragonDetailedStatsTab } from '../pages/swimming-dragon-detailed-stats/swimming-dragon-detailed-stats'
 
-import { YinYangTimer } from '../components/yin-yang/yin-yang'
+import { NotebookPage } from '../pages/notebook/notebook';
+import { NotesPage } from '../pages/notes/notes';
 
+/* Providers */
 import { Timers } from '../providers/timers/timers';
 import { Forms } from '../providers/forms';
 import { SoundPlayer } from '../providers/sound-player';
 import { OrientationStore } from '../providers/orientation-store';
 import { Dropbox } from '../providers/dropbox';
 
-import { InAppBrowser } from '@ionic-native/in-app-browser';  
-import { LoginPage } from '../pages/login-page/login-page';
-
 @NgModule({
   declarations: [
-    MyApp, LoginPage,
+    MyApp,
     HomePage, AboutPage,
-    SwimmingDragonPage, 
-      SwimmingDragonTimerTab, SwimmingDragonSummaryStatsTab, SwimmingDragonDetailedStatsTab,
     TimersPage, TimerPage, TimerConfigPage,
     FormsPage, FormPage, 
+    NotebookPage, NotesPage,
+    SwimmingDragonPage, 
+      SwimmingDragonTimerTab, 
+      SwimmingDragonSummaryStatsTab, 
+      SwimmingDragonDetailedStatsTab,
+    
     YinYangTimer
 
   ],
   imports: [
+    ElasticModule,
     IonicModule.forRoot(MyApp),
     IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp, LoginPage,
+    MyApp,
     HomePage, AboutPage,
-    SwimmingDragonPage, 
-      SwimmingDragonTimerTab, SwimmingDragonSummaryStatsTab, SwimmingDragonDetailedStatsTab,
     TimersPage, TimerPage, TimerConfigPage,
-    FormsPage, FormPage
+    FormsPage, FormPage,
+    NotebookPage, NotesPage,
+    SwimmingDragonPage, 
+      SwimmingDragonTimerTab,
+      SwimmingDragonSummaryStatsTab,
+      SwimmingDragonDetailedStatsTab
   ],
   providers: [
-    StatusBar, SplashScreen, NativeAudio, Device,
+    AppVersion, Device, Dialogs, InAppBrowser,
+    NativeAudio, SplashScreen, StatusBar,
     Timers, Forms,
     SoundPlayer,
     OrientationStore,
-    Dropbox, InAppBrowser,
+    Dropbox, 
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
