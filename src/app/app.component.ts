ebc579c4dbec3a22eb4b5700ec9d58f25c09a7e0
Chris Davies <Christopher.James.Davies@gmail.com>
2017-05-01 08:15:48 +0100

    Changed back to HomePage as Home Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { AboutPage } from '../pages/about/about';
 
-import { LoginPage } from '../pages/login-page/login-page';
-
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = LoginPage;
+  rootPage: any = HomePage;
   aboutPage: any = AboutPage;
 
   constructor(
