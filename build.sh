d126d70f5ac24d3e079b6cd102cfb788640a6cb0
Chris Davies <Christopher.James.Davies@gmail.com>
2017-05-01 08:06:50 +0100

    Update Framework
       - Add Native Modules : AppVersion and Dialogs
       - Added Webpack building for Android Apps

--- build.sh
+++ build.sh
@@ -4,17 +4,47 @@ ionic platform remove browser
 ionic platform remove android
 ionic platform remove windows
 
-ionic platform add windows
-ionic platform add browser
-ionic platform add android
+ionic plugin remove --save cordova-plugin-device
+ionic plugin remove --save cordova-plugin-nativeaudio
+ionic plugin remove --save cordova-sqlite-storage
+ionic plugin remove --save cordova-plugin-splashscreen
+ionic plugin remove --save cordova-plugin-statusbar
+ionic plugin remove --save cordova-plugin-inappbrowser
 
-ionic plugin add cordova-plugin-device
+npm uninstall --save @ionic-native/core
+npm uninstall --save @ionic-native/device
+npm uninstall --save @ionic-native/device-orientation
+npm uninstall --save @ionic-native/in-app-browser
+npm uninstall --save @ionic-native/media
+npm uninstall --save @ionic-native/native-audio
+npm uninstall --save @ionic-native/splash-screen 
+npm uninstall --save @ionic-native/status-bar
+npm uninstall --save @ionic/storage
+ionic cache clean
+
+ionic platform add windows browser  android
+
+ionic plugin add --save cordova-plugin-device
+ionic plugin add --save cordova-plugin-nativeaudio
+ionic plugin add --save cordova-sqlite-storage
+ionic plugin add --save cordova-plugin-splashscreen
+ionic plugin add --save cordova-plugin-statusbar
+ionic plugin add --save cordova-plugin-inappbrowser
+ionic plugin add --save cordova-plugin-dialogs
+ionic plugin add --save cordova-plugin-app-version
+
+npm install --save @ionic-native/core
 npm install --save @ionic-native/device
-
-ionic plugin add cordova-plugin-nativeaudio
+npm install --save @ionic-native/device-orientation
+npm install --save @ionic-native/in-app-browser
+npm install --save @ionic-native/media
 npm install --save @ionic-native/native-audio
-
-cordova plugin add cordova-sqlite-storage --save
+npm install --save @ionic-native/splash-screen 
+npm install --save @ionic-native/status-bar
+npm install --save @ionic-native/dialogs
+npm install --save @ionic-native/app-version
 npm install --save @ionic/storage
+
+npm install --save angular2-elastic
 
 
